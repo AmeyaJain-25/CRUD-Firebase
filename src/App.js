bedc575b8583eab10784b345607538aab4226d63
AmeyaJain-25 <ameiya.jain@hotmail.com>
2021-04-11 15:25:29 +0530

    Navbar done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+//Firebase
+import firebase from "firebase";
+import "firebase/auth";
+import { useState } from 'react';
+import { UserContext } from './context/UserContext';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Header from './layout/Header';
 
-function App() {
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+
   return (
-    <div className="App">
-      <h1>
-        Crud - Firebase
-      </h1>
-    </div>
+    <Router>
+      <UserContext.Provider value={{user, setUser}}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="*" component={Home} />
+        </Switch>
+      </UserContext.Provider>
+    </Router>
   );
 }
 
---
+++ src/layout/Header.js
@@ -0,0 +1,34 @@
+import { Button } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
+const Header = () => {
+    const context  = useContext(UserContext);
+
+    return (
+        <div style={{padding: "10px", backgroundColor: "blanchedalmond", display: "flex", justifyContent: "space-between"}}>
+            <div>
+                <h1>CRUD-Firebase</h1>
+            </div>
+            <div>
+                {context.user ? (
+                    <Link to="/">
+                        <Button style={{margin: "5px 10px"}}>Logout</Button>
+                    </Link>
+                    ) : (
+                    <>
+                        <Link to="/signin">
+                        <Button style={{margin: "5px 10px"}}>Signin</Button>
+                        </Link>
+                        <Link to="/signup">
+                            <Button style={{margin: "5px 10px"}}>Signup</Button>
+                        </Link>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Header;
