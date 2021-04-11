91624bb1457ea30a270e66c6bbab458421531318
AmeyaJain-25 <ameiya.jain@hotmail.com>
2021-04-11 16:18:20 +0530

    Basic Account Creation done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Redirect } from "react-router";
+import { UserContext } from "../context/UserContext";
 
 const Home = () => {
+    const context = useContext(UserContext);
+
+    if (!context.user?.uid) {
+        return <Redirect to="/signin" />;
+    }
+
     return (
         <div>
-            Home
+            <h1>{context.user.uid}</h1>
+            <h1>{context.user.email}</h1>
         </div>
     );
 }
